312531bd8800e9bcc3a8e5c39cc6106eff971f94
uvaaland <ubvaaland@gmail.com>
2018-01-19 15:57:06 -0500

    Add ReadFileToken function and test

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,9 +1,21 @@
 import pytest
+import os
 from context import utils
+
+
+## Pytest Fixtures
+
+
+@pytest.fixture
+def prefix():
+    "Returns the path prefix"
+    cwd = os.path.basename(os.getcwd())
+    return "files/" if cwd == "test" else "test/files/"
 
 
 ## Unittests
 
 
-def test_Example():
-    assert True == False
+def test_ReadFileToken(prefix):
+    token = utils.ReadFileToken(token = prefix + "token/token.txt")
+    assert token == "123456789:aBcDeFgH"
--- utils.py
+++ utils.py
@@ -1,5 +1,25 @@
-class ExampleClass:
+import os
+import sys
+
+
+def ReadFileToken(token="token/token.txt"):
+    with open(token, 'r') as f:
+        return f.read().rstrip('\n')
+
+
+def ReadFileURL(urlfile):
+    with open(urlfile, 'r') as f:
+        pass
+        
+
+def WriteFileURL():
     pass
 
-def ExampleFunction():
+def WriteFileRequest():
+    pass
+
+def RequestURL():
+    pass
+
+def RequestToDataframe():
     pass
