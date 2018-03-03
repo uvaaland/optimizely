e8e8545f1b030ba1c20283d78cffc4ba687e70d6
uvaaland <ubvaaland@gmail.com>
2018-03-03 11:44:34 -0500

    Add Logger class

--- optimizely.py
+++ optimizely.py
@@ -29,12 +29,28 @@ import asyncio
 from aiohttp import ClientSession, ContentTypeError
 from requests import Session
 import pandas as pd
+import time
+import sys
 
 
 # Print formatting
 HEADER = "{0:^80}"
 STATUS_BAR = "{0:^10}{1:^60}{2:^10}"
 HORIZONTAL_RULE = "-"*80
+
+
+class Logger:
+
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("{}.log".format(time.strftime("%Y-%m-%d")), "a")
+        
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
 
 
 async def fetch_all(urls, token):
@@ -65,7 +81,7 @@ async def fetch_all(urls, token):
         return responses
 
 
-async def fetch(url, token, session, tracker, verbose=True):
+async def fetch(url, token, session, tracker):
     """Fetch a url asynchronously, using specified ClientSession.
 
     Makes a request for the given url and extracts status and json content of
@@ -78,7 +94,6 @@ async def fetch(url, token, session, tracker, verbose=True):
         session: A ClientSession object used to make requests.
         tracker: A list containing the current iteration and the total number
             of iterations.
-        verbose: Optional argument for printing progress to terminal.
 
     Returns:
         A dictionary that maps the url to a tuple containing the request status
@@ -93,10 +108,10 @@ async def fetch(url, token, session, tracker, verbose=True):
             content = []
         status = response.status
 
-        if verbose:
-            tracker[0] = tracker[0] + 1
-            count = "[{0:03}/{1:03}]:".format(tracker[0], tracker[1])
-            print(STATUS_BAR.format(count, ".../" + url[31:], str(status == 200)))
+        # Print status
+        tracker[0] = tracker[0] + 1
+        count = "[{0:03}/{1:03}]:".format(tracker[0], tracker[1])
+        print(STATUS_BAR.format(count, ".../" + url[31:], str(status == 200)))
 
         return {url : (status, content)}
 
@@ -125,7 +140,7 @@ def _get_requests_async(urls, token):
     return {k : v for d in responses for (k, v) in d.items()}
 
 
-def _get_requests_sync(urls, token, verbose=True):
+def _get_requests_sync(urls, token):
     """Fetch list of web pages sequentially.
 
     Fetches a list of web pages sequentially and extracts the request status
@@ -134,7 +149,6 @@ def _get_requests_sync(urls, token, verbose=True):
     Args:
         urls: A list of web pages.
         token: A string with token that is needed to make requests.
-        verbose: Optional argument for printing progress to terminal.
 
     Returns:
         A dictionary that maps url keys to a tuple containing the status and
@@ -156,14 +170,14 @@ def _get_requests_sync(urls, token, verbose=True):
 
         responses[url] = (status, content)
 
-        if verbose:
-            count = "[{0:03}/{1:03}]:".format(i+1, len(urls))
-            print(STATUS_BAR.format(count, ".../" + url[31:], str(status == 200)))
+        # Print status
+        count = "[{0:03}/{1:03}]:".format(i+1, len(urls))
+        print(STATUS_BAR.format(count, ".../" + url[31:], str(status == 200)))
 
     return responses
 
 
-def get_requests(urls, token, async=True, verbose=True):
+def get_requests(urls, token, async=True):
     """Launches the fetching of web pages in either asynchronous or sequential mode.
 
     Takes in a list of web pages and a token and launches a routine for
@@ -174,7 +188,6 @@ def get_requests(urls, token, async=True, verbose=True):
         urls: A list of web pages.
         token: A string with token that is needed to make requests.
         async: Optional argument for running in asynchronous mode.
-        verbose: Optional argument for printing progress to terminal.
 
     Returns:
         A dictionary that maps url keys to a tuple containing the status and
@@ -183,9 +196,9 @@ def get_requests(urls, token, async=True, verbose=True):
         {"https://example.com" : (200, [{"name" : "John", "city" : "New York"};])
     """
 
-    if verbose:
-        print(STATUS_BAR.format("#", "REQUEST (https://www.optimizelyapis.com/...)", "SUCCESS"))
-        print(HORIZONTAL_RULE)
+    # Print status
+    print(STATUS_BAR.format("#", "REQUEST (https://www.optimizelyapis.com/...)", "SUCCESS"))
+    print(HORIZONTAL_RULE)
 
     start_time = default_timer()
     if async:
@@ -194,10 +207,10 @@ def get_requests(urls, token, async=True, verbose=True):
         responses = _get_requests_sync(urls, token)
     elapsed_time = default_timer() - start_time
 
-    if verbose:
-        print(HORIZONTAL_RULE)
-        print("Elapsed time: {0:63.2f}s".format(elapsed_time))
-        print(HORIZONTAL_RULE)
+    # Print status
+    print(HORIZONTAL_RULE)
+    print("Elapsed time: {0:63.2f}s".format(elapsed_time))
+    print(HORIZONTAL_RULE)
 
     return responses
 
@@ -252,7 +265,7 @@ def _generate_urls(dataframe, target):
     return ["https://www.optimizelyapis.com/" + target_url.format(i) for i in ids]
 
 
-def generate_url_files(dataframe, targets, verbose=True):
+def generate_url_files(dataframe, targets):
     """Writes a file of urls for the given targets.
 
     Takes in a dataframe and a list of targets and generates a url file for
@@ -263,7 +276,6 @@ def generate_url_files(dataframe, targets, verbose=True):
         dataframe: A dataframe containing information from a set of web pages.
         targets: A list of strings with the name for which a url file should be
             generated.
-        verbose: Optional argument for printing progress to terminal.
 
     Returns:
         None
@@ -276,11 +288,11 @@ def generate_url_files(dataframe, targets, verbose=True):
             for url in urls:
                 outfile.write(url + '\n')
 
-        if verbose:
-            print("Saved '{0}' urls to 'urls/{0}.url'.".format(target))
+        # Print status
+        print("Saved '{0}' urls to 'urls/{0}.url'.".format(target))
 
 
-def main(verbose=True):
+def main():
     """Pulls data for every project, experiment, stat, and variation and writes
     it to file.
 
@@ -289,11 +301,14 @@ def main(verbose=True):
     files and can be found in the output/ folder.
 
     Args:
-        verbose: Optional argument for printing progress to terminal.
+        None
 
     Returns:
         None
     """
+
+    logger = Logger()
+    sys.stdout = logger
 
     with open("token/token.txt", 'r') as infile:
         token = infile.read().rstrip('\n')
@@ -301,10 +316,10 @@ def main(verbose=True):
     parameters = ["projects", "experiments", "stats", "variations"]
     for par in parameters:
 
-        if verbose:
-            print(HORIZONTAL_RULE)
-            print(HEADER.format(par.upper()))
-            print(HORIZONTAL_RULE)
+        # Print status
+        print(HORIZONTAL_RULE)
+        print(HEADER.format(par.upper()))
+        print(HORIZONTAL_RULE)
 
         with open("urls/{}.url".format(par), 'r') as infile:
             urls = infile.read().splitlines()
@@ -324,16 +339,16 @@ def main(verbose=True):
         # Write to file
         dataframe.to_csv("output/{}.csv".format(par), index=False)
 
-        if verbose:
-            print("Saved '{0}' frame to 'output/{0}.csv'.".format(par))
+        # Print status
+        print("Saved '{0}' frame to 'output/{0}.csv'.".format(par))
 
         if par == "projects":
             generate_url_files(dataframe, ["experiments"])
         elif par == "experiments":
             generate_url_files(dataframe, ["stats", "variations"])
 
-        if verbose:
-            print(HORIZONTAL_RULE)
+        # Print status
+        print(HORIZONTAL_RULE)
 
 
 if __name__ == "__main__":
